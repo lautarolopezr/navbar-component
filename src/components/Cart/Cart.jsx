7c9e4247b0cf714cf2825a9a9097df1ad62c629f
Lautaro <lautarolopezr@gmail.com>
2023-03-02 16:21:06 -0300

    fixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ItemCart from "../ItemCart/ItemCart";
+import ItemList from "../ItemLIst/ItemList";
+
 
 const Cart = () => {
-    const condLogica = false
+    
+    const cart = [
+        {id : 1 , name : "Rice" , price : 400 , img: "apple-tv.jpg", ammount : 4 },
+        {id : 2 , name : "Noodles" , price : 200 , img: "apple-tv.jpg" , ammount : 5 },
+        {id : 3 , name : "Soap" , price : 100 , img : "apple-tv.jpg" ,ammount : 6 }
+
+    ]
+
+    
+    
     return (
         <>
-        {condLogica ? <h1>Hola</h1> : <h1>Chau</h1>}
+        {
+            cart.length === 0 
+            ?
+            <>
+            <p>Cart is empty</p>
+            <Link><button className="btn btn-dark">Back to Home</button></Link>
+            </>
+
+            :
+
+            <div className="container cartContainer">
+
+                <ItemList products={cart} template={"itemCart"}/>    
+            
+            <div className="divButtons">
+                <p>Sumamry of purchase</p>
+                <button className="btn btn-dark" onClick={()=> "Vaciar"}>Empty Cart</button>
+                <Link to={"/"}><button className="btn btn-dark">Continue buying</button></Link>
+                <Link to={"/checkout"}><button className="btn btn-dark">Finalize Purchase</button></Link>
+
+            </div>
+
+
+            </div>
+
+        }
         </>
     )
 
--- src/components/ItemCart/ItemCart.jsx
+++ src/components/ItemCart/ItemCart.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const ItemCart = () => {
+const ItemCart = ({item}) => {
     return (
-        <div>
-            <h1>Hola</h1>
+        <div className='card mb-3 cardCart'>
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src={`/img/${item.img}`} alt="" className='img-fluid rounded-start' />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className='card-title'>{item.name}</h5>
+                        <p className='card-text'>Ammount : {item.ammount}</p>                        
+                        <p className='card-text'>Unit Price : ${new Intl.NumberFormat("de-DE").format(item.price)}</p>
+                        <p className='card-text'>Subtotal : ${new Intl.NumberFormat("de-DE").format(item.price * item.ammount)}</p>
+                        <button className='btn btn-danger' onClick={() => "Borrar Producto"}>Delete Cart</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
